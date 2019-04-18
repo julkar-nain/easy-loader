2aba3665355ccd0dcbc38ee1ed395d8eecbb571e
julkar nain <julkarnaincse@gmail.com>
2019-04-18 18:20:33 +0600

    Show two sample image

--- sample/src/main/java/com/nain/easyloader/MainActivity.kt
+++ sample/src/main/java/com/nain/easyloader/MainActivity.kt
@@ -10,14 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sampleImageUrl = "https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=1881cd689e10e5dca28839e68678f432"
+        val sampleImageUrlForUpper = "https://images.unsplash.com/photo-1464547323744-4edd0cd0c746?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=c990dc1cd803124b9e6c43b97c844ad3"
+        val sampleImageUrlForLower = "https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=1881cd689e10e5dca28839e68678f432"
 
         EasyLoader
             .imageBuilder()
-            .src(sampleImageUrl)
+            .src(sampleImageUrlForUpper)
             .placeholder(R.drawable.placeholder)
-            .container(imageView)
+            .container(imageViewUpper)
             .build()
             .show()
+
+        EasyLoader
+            .imageBuilder()
+            .src(sampleImageUrlForLower)
+            .placeholder(R.drawable.placeholder)
+            .container(imageViewLower)
+            .build()
+            .show()
+
     }
 }
